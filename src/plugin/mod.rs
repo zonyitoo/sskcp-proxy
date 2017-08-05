eebdd709501cc65a201a75499172be6ff5717d67
Y. T. Chung <zonyitoo@gmail.com>
2017-08-05 17:08:02 +0800

    refactor plugin start logic

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -67,33 +67,54 @@ pub fn launch_plugin(config: &mut Config, mode: PluginMode) -> io::Result<Option
         let loop_ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
         let local_addr = SocketAddr::new(loop_ip, get_local_port()?);
 
-        let ref orig_svr_addr = config.remote_addr;
-        let svr_addr = match start_plugin(c, orig_svr_addr, &local_addr, mode) {
-            Err(err) => {
-                panic!("Failed to start plugin \"{}\", err: {}", c.plugin, err);
-            }
-            Ok(p) => {
-                let svr_addr = ServerAddr::SocketAddr(local_addr);
-                plugin = Some(Plugin {
-                                  addr: svr_addr.clone(),
-                                  process: p,
-                              });
+        let svr_addr = match mode {
+            PluginMode::Client => {
+                // Client plugin will listen on local and relay to remote
+                // So we allocate a loopback address for plugin's local to listen, and then set our remote address
+                // to the newly allocated address
+                let ref remote_addr = config.remote_addr;
+                info!("Started plugin \"{}\" on {} <-> {}", c.plugin, local_addr, remote_addr);
+                match start_plugin(c, remote_addr, &local_addr, mode) {
+                    Err(err) => {
+                        panic!("Failed to start plugin \"{}\", err: {}", c.plugin, err);
+                    }
+                    Ok(p) => {
+                        let svr_addr = ServerAddr::SocketAddr(local_addr);
+                        plugin = Some(Plugin {
+                                          addr: svr_addr.clone(),
+                                          process: p,
+                                      });
 
-                // Replace addr with plugin
-                svr_addr
+                        // Replace addr with plugin
+                        svr_addr
+                    }
+                }
+            }
+            PluginMode::Server => {
+                // Server plugin will listen on remote and relay to local
+                // So we allocate a loopback address for plugin's remote to listen, and then set our local address
+                // to the newly allocated address
+                let svr_addr = ServerAddr::SocketAddr(local_addr);
+                let local_addr = config.local_addr.listen_addr();
+                info!("Started plugin \"{}\" on {} <-> {}", c.plugin, svr_addr, local_addr);
+                match start_plugin(c, &svr_addr, local_addr, mode) {
+                    Err(err) => {
+                        panic!("Failed to start plugin \"{}\", err: {}", c.plugin, err);
+                    }
+                    Ok(p) => {
+                        plugin = Some(Plugin {
+                                          addr: svr_addr.clone(),
+                                          process: p,
+                                      });
+
+                        // Replace addr with plugin
+                        svr_addr
+                    }
+                }
             }
         };
 
-        match mode {
-            PluginMode::Client => {
-                info!("Started plugin \"{}\" on {} <-> {}", c.plugin, local_addr, orig_svr_addr);
-                svr_addr_opt = Some(svr_addr); // Fuck borrow checker
-            }
-            PluginMode::Server => {
-                info!("Started plugin \"{}\" on {} <-> {}", c.plugin, orig_svr_addr, local_addr);
-                svr_addr_opt = Some(orig_svr_addr.clone()); // Fuck borrow checker
-            }
-        }
+        svr_addr_opt = Some(svr_addr);
     }
 
     if let Some(svr_addr) = svr_addr_opt {
